d620db96c789856c039ad7535b52659dbc922cd2
zsg0604 <1005227857@qq.com>
2019-09-17 18:28:48 +0800

    zsg---11

---
+++ api-demo/src/main/java/com/jk/model/Book.java
@@ -0,0 +1,11 @@
+package com.jk.model;
+
+/**
+ * @version :   1.0.0
+ * @Author :  Zhangsongguang
+ * @CreateDate :   2019/9/17 18:27
+ * @Description :   书籍
+ * @Package :   com.jk.model
+ */
+public class Book {
+}
