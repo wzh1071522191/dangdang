2f807fedd37ab31adacbdb76fe5a93dec184546b
zsg0604 <1005227857@qq.com>
2019-09-18 10:01:42 +0800

    0918张松光第一次上传

--- api-demo/src/main/java/com/jk/model/Book.java
+++ api-demo/src/main/java/com/jk/model/Book.java
@@ -1,5 +1,11 @@
 package com.jk.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 /**
  * @version :   1.0.0
  * @Author :  Zhangsongguang
@@ -7,5 +13,40 @@ package com.jk.model;
  * @Description :   书籍
  * @Package :   com.jk.model
  */
+@Data
 public class Book {
+
+    private Integer bookId;
+
+    private String bookName;
+
+    private String bookAuthor;
+
+    private Integer bookTypeId;
+
+    private String press;
+
+    private String bookContent;
+
+    private Double bookPrice;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date bookDate;
+
+    private Integer bookStatus;
+
+    private Integer bookCount;
+
+    private Double hits;
+
+    private Integer bookStar;
+
+    private String bookImg1;
+
+    private String bookImg2;
+
+    private String bookImg3;
+
+    private BookType bookType;
 }
--- zsg-client/src/main/java/com/jk/service/BookServiceImpl.java
+++ zsg-client/src/main/java/com/jk/service/BookServiceImpl.java
@@ -1,8 +1,14 @@
 package com.jk.service;
 
 import com.jk.dao.BookDao;
+import com.jk.model.Book;
+import com.jk.util.ParameUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @version :   1.0.0
@@ -18,4 +24,15 @@ public class BookServiceImpl implements BookServiceApi{
     private BookDao bookDao;
 
 
+    @Override
+    public HashMap<String, Object> queryBook(ParameUtil parameUtil) {
+        Integer page = parameUtil.getPageNumber();
+        parameUtil.setPageNumber((page-1)*parameUtil.getPageSize());
+        Integer total = bookDao.queryBookCount();
+        List<Book> queryBookList =bookDao.queryBookList(parameUtil);
+        HashMap<String, Object> hashMap =new HashMap<String, Object>();
+        hashMap.put("total",total);
+        hashMap.put("rows",queryBookList);
+        return hashMap;
+    }
 }
