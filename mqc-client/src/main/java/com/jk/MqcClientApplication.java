82726a5c4830636f18c879229f1ae55df6a40db5
QingChao-M <1764314782@qq.com>
2019-09-20 16:21:18 +0800

    胆战心惊的上传《2》

--- mqc-client/src/main/java/com/jk/MqcClientApplication.java
+++ mqc-client/src/main/java/com/jk/MqcClientApplication.java
@@ -1,11 +1,15 @@
 package com.jk;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@MapperScan("com.jk.dao")
 public class MqcClientApplication {
 
     public static void main(String[] args) {
